5900086f930b02c0771909bab40704b8f5e17aff
Dušan Baran <work.dusanbaran@gmail.com>
2018-08-03 09:41:51 +0200

    Add default settings paths

--- lib/cloudkeeper/aws/settings.rb
+++ lib/cloudkeeper/aws/settings.rb
@@ -6,6 +6,12 @@ module Cloudkeeper
     class Settings < Settingslogic
       CONFIGURATION = 'cloudkeeper-aws.yml'.freeze
 
+      source "#{ENV['HOME']}/.cloudkeeper-aws/#{CONFIGURATION}" \
+      if File.exist?("#{ENV['HOME']}/.cloudkeeper-aws/#{CONFIGURATION}")
+
+      source "/etc/cloudkeeper-aws/#{CONFIGURATION}" \
+      if File.exist?("/etc/cloudkeeper-aws/#{CONFIGURATION}")
+
       source "#{File.dirname(__FILE__)}/../../../config/#{CONFIGURATION}"
     end
   end
